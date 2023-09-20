2d49d7d734101d863ee4c1e6e803b597a423dc39
Barzy Bahadin <80420497+BarziBahadin@users.noreply.github.com>
2023-09-20 14:42:21 +0300

    after ep 2 and 3

--- app.js
+++ app.js
@@ -1,22 +1,44 @@
+let i = 1;
 const app = Vue.createApp({
-    // data and fuctions from a templayes
+  // data and fuctions from a templayes
 
-   // template: '<h2>I am a template</h2>'
+  // template: '<h2>I am a template</h2>'
 
-   data(){ // to output dynamic data to the file in the html 
+  data() {
+    // to output dynamic data to the file in the html
     return {
-        showBooks : true,
-        title: "deep work ",
-        author : "call newport",
-        type: "self help",    
-        age : 45
-    }
-   },
-   methods: {
-    toggleShowBooks(){
-        this.showBooks = !this.showBooks
-    }
-   }
-})
-app.mount("#app")
+      showBooks: true,
+      url: "https://www.google.com",
+      books: [
+        { title: "name of the wind", author: "patrick roth's" ,img:'assets/1.jpg', isFave:true},
+        { title: "the way of kings", author: "brandon sanders ", img:'assets/2.jpg',isFave:false},
+        { title: "the final empire", author: "brandon   sanders ",img:'assets/3.jpg',isFave:true },
+      ],
+    };
+  },
+  methods: {
+    addFave(book){
+   book.isFave = !book.isFave
+    },
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
+    },
+    handleEvent(e) {
+      i++;
+      console.log("event " + i);
+      console.log(e.type);
+    },
+    handleMouseMove(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
+    },
+  },
+  computed:{
+    filteredBooks(){
+        return this.books.filter((book) => book.isFave)
 
+    }
+  }
+});
+
+app.mount("#app");
