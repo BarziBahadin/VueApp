1f546665e70e977e408f70e7659a26d0820bc426
Barzy Bahadin <80420497+BarziBahadin@users.noreply.github.com>
2023-09-21 08:50:32 +0300

    vue CLI

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+   return{
+     title: "my fist VUE app :D "
+
+   }
   }
 }
 </script>
@@ -22,5 +24,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1{
+  border-bottom: 1px solid #0000;
+display: inline-block;
+padding-bottom: 10px;
+
 }
 </style>
