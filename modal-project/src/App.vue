2c28fbb474952941408586437c85fecda4500395
Barzy Bahadin <80420497+BarziBahadin@users.noreply.github.com>
2023-10-12 22:04:34 +0300

    adding view  router

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,73 +1,32 @@
 <template>
   <div>
-    <h1>{{ title }}</h1>
-    <hr>
-    <p>welcome...</p>
-    <teleport to=".modals" v-if="showModal">
-      <Modal @close="toggleModal" :header="header" :text="text" theme=''>
-        <template v-slot:links>
-          <a href="#">sing up now</a>
-          <a href="#">more info</a>
-        </template>
-      </Modal>
-    </teleport>
-    <teleport to=".modals" v-if="showModalTow">
-
-
-      <Modal @close="toggleModalTow" theme='sale'>
-        <h2>its on sale</h2>
-        <template v-slot:links>
-
-          <a href="#">you have a discount</a>
-          <a href="#">more info of sale</a>
-        </template>
-      </Modal>
-    </teleport>
-    <button @click="toggleModal">open model (ALT)</button>
-    <button @click="toggleModalTow">sale</button>
+      <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/> 
   </div>
 </template>
 
-<script>
-import Modal from "./components/Modal.vue"
-
-export default {
-  name: 'App',
-  components: { Modal },
-  data() {
-    return {
-      title: "my fist VUE app :D ",
-      header: "sing up for the game",
-      text: "you can win 100000$ ",
-      showModal: false,
-      showModalTow: false
-    }
-  },
-  methods: {
-    toggleModal() {
-      this.showModal = !this.showModal
-    },
-    toggleModalTow() {
-      this.showModalTow = !this.showModalTow
-    }
-  }
-}
-</script>
-
 <style>
-#app,
-.modals {
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
-h1 {
-  border-bottom: 1px solid #0000;
-  display: inline-block;
-  padding-bottom: 10px;
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
